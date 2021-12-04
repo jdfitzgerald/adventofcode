11e3fc0cbe336e0c9ce5c47a881efee7af80a72d
jd <jd@jdf.ie>
2021-12-04 18:07:23 +0000

    tidy

--- 3/3.py
+++ 3/3.py
@@ -1,33 +1,74 @@
 file = open('test','r')
-#file = open('data','r')
+file = open('data','r')
 
 x=0
 
+n1=[]
+n0=[]
+ones=0
+
+v='' 
+
 for index,line in enumerate(file):
 	v=line.strip()
-	#n = int(v, 2)
-
-	if index==0: 
-		totals=[]
-		for i in range(len(v)):
-			totals.append(0)
-
-	for i in range(len(v)):
-		totals[i] = totals[i] + int(v[i])
-	print(totals)
-
-target = index/2
-gamma = ''
-epsilon = ''
-for t in totals:
-	if t > target:
-		gamma+='1'
-		epsilon+='0'
+	if v[0]=='1':
+		n1.append(v)
+		ones+=1
 	else:
-		gamma+='0'
-		epsilon+='1'
+		n0.append(v)
 
-power = int(gamma, 2) * int(epsilon,2)
-print(gamma, epsilon)
-print(power)
+nbits = len(v)
+	
+if ones >= index/2:
+	oxy = n1
+	co2 = n0
+else:
+	oxy = n0
+	co2 = n1
 
+for i in range(1,nbits):
+	n1=[]
+	n0=[]
+	ones=0
+	for v in oxy:
+		if v[i]=='1':
+			n1.append(v)
+			ones+=1
+		else:
+			n0.append(v)
+
+	if ones >= len(oxy)/2:
+		oxy = n1
+	else:
+		oxy = n0
+	
+	if len(oxy) == 1:
+		break;
+
+for i in range(1,nbits):
+	n1=[]
+	n0=[]
+	ones=0
+	print(co2)
+	for v in co2:
+		if v[i]=='1':
+			n1.append(v)
+			ones+=1
+		else:
+			n0.append(v)
+
+	print(ones, len(co2), n0, n1)
+	if ones >= len(co2)/2:
+		co2 = n0
+	else:
+		co2 = n1
+	
+	if len(co2) == 1:
+		break;
+
+
+print(oxy)
+print(co2)
+ans = int(oxy[0],2)*int(co2[0],2)
+
+print(ans)
