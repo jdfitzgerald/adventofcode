a3d82cbb4d4d50a2ecc781fff12da19d3a4c7e5c
jd <jd@jdf.ie>
2021-12-07 13:54:00 +0000

    failed attempt

--- 7/7.py
+++ 7/7.py
@@ -2,5 +2,17 @@ import pprint
 pp = pprint.PrettyPrinter(indent=2)
 
 file = open('test','r')
-#file = open('data','r')
+file = open('data','r')
+
+line = file.readline()
+crabs = [int(x) for x in line.strip().split(',')]
+
+max_pos = max(crabs)+1;
+costs=[0]*max_pos
+for pos in range(max_pos):
+	for crab in crabs:
+		costs[pos] += abs(pos - crab)
+
+print(min(costs), costs.index(min(costs)))
+	
 
