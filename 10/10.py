c57e2d337bc9675e61685c31e498c83888461f59
jd <jd@jdf.ie>
2021-12-10 09:10:32 +0000

    10.1

--- 10/10.py
+++ 10/10.py
@@ -1,6 +1,28 @@
 import pprint 
 pp = pprint.PrettyPrinter(indent=2)
 
-file = open('test','r')
-#file = open('data','r')
+#file = open('test','r')
+file = open('data','r')
 
+closers = {
+	')':('(',3),
+	']':('[',57),
+	'}':('{',1197),
+	'>':('<',25137)
+}
+
+score = 0
+for line in file:
+	stack = []
+	for bracket in line.strip():
+		if bracket in closers.keys():
+			if stack.pop() != closers[bracket][0]:
+				score += closers[bracket][1]
+				break;
+		else: 
+			stack.append(bracket)
+
+print(score)
+
+
+		
