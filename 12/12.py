7296fee98330581e84d8868615bea8b7248fff31
jd <jd@jdf.ie>
2021-12-12 15:57:37 +0000

    part 1

--- 12/12.py
+++ 12/12.py
@@ -1,6 +1,42 @@
 import pprint 
 pp = pprint.PrettyPrinter(indent=2)
 
-file = open('test','r')
-#file = open('data','r')
+file = open('test3','r')
+file = open('data','r')
 
+graph = {}
+
+def add_edge(a,b):
+	if a not in graph:
+		graph[a] = []
+	if b not in graph:
+		graph[b] = []
+	
+	graph[a].append(b)
+	graph[b].append(a)
+
+for line in file:
+	add_edge(*line.strip().split('-'))
+
+pp.pprint(graph)
+
+paths = []
+
+def find_paths(node,visited=[]):
+	visited.append(node)
+
+	#print('at %s kids '%node, graph[node])
+	#print('visited ', visited)
+
+	if node == 'end': 
+		paths.append(visited)
+		return
+
+
+	for n in graph[node]:
+		if n not in visited or n.isupper():
+			find_paths(n, visited.copy())
+	
+find_paths('start')
+pp.pprint(paths)
+pp.pprint(len(paths))
