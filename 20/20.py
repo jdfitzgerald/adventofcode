ce4d24dc9feb0f36d0b7be38ca49f4a7640f7db2
jd <jd@jdf.ie>
2021-12-20 13:30:53 +0000

    pt 2 done

--- 20/20.py
+++ 20/20.py
@@ -5,50 +5,59 @@ pp = pprint.PrettyPrinter(indent=2)
 # 5382 guess too high
 # 4962 too low
 
+# p2 32028 too high
+
 #file = open('test','r')
 file = open('data','r')
 
-algo = next(file).strip().replace('#','1').replace('.','0')
+algo = next(file).strip()
 next(file)
 
 image=[]
 for line in file:
-	image.append(line.strip().replace('#','1').replace('.','0'))
+	image.append(line.strip())
 
 def pad(image):
 	size_y = len(image)+2
 	size_x = len(image[0])+2
-	empty = '0'*size_x
+	empty = '.'*size_x
 
 	result = []
 	result.append(empty)
-	for line in image: result.append('0'+line+'0')
+	for line in image: result.append('.'+line+'.')
 	result.append(empty)
 	return result
 
+def discard_outer(image):
+	result = []
+	for y in range(1,len(image)-1):
+		result.append(image[y][1:-1])
+	return result
+
+
 def enhance(image,algo):
 	image = pad(image)
-	result = image.copy()
+	result = []
 	for y in range(1, len(image) -1):
-		result[y] = '0'
+		result.append('')
 		for x in range(1, len(image[0]) -1):
-			bstr = image[y-1][x-1:x+2] + image[y][x-1:x+2] + image[y+1][x-1:x+2] 
-			result[y]+=algo[int(bstr,2)]
-		result[y] += '0'
+			bstr = (image[y-1][x-1:x+2] + image[y][x-1:x+2] + image[y+1][x-1:x+2]).replace('#','1').replace('.','0')
+			result[y-1]+=algo[int(bstr,2)]
+	
 	return result
 
 # initial pad required
-image = pad(image)
+iterations = 50
+for i in range(iterations):
+	image = pad(image)
+	image = pad(image)
+	image = pad(image)
+	image = pad(image)
 
-for line in image: print(line,sum([int(c) for c in line]))
-	
-image = enhance(image,algo)
+for i in range(iterations):
+	image = enhance(image,algo)
+	image = discard_outer(image)
 
-for line in image: print(line,sum([int(c) for c in line]))
-	
-image = enhance(image,algo)
 
-for line in image: print(line,sum([int(c) for c in line]))
-	
 
-print(sum([int(c) for line in image for c in line]))
+print(sum([line.count('#') for line in image]))
