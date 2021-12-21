c8157d59beb237f91829bf92d555e3db89868d07
jd <jd@jdf.ie>
2021-12-21 00:27:31 +0000

    works for test but janky, rework for data

--- 19/19.py
+++ 19/19.py
@@ -2,11 +2,16 @@ import pprint
 pp = pprint.PrettyPrinter(indent=2)
 
 file = open('test','r')
-#file = open('data','r')
+SCANNERS = 5
+file = open('data','r')
+SCANNERS = 26
+
+routes = []
+for i in range(SCANNERS): routes.append([])
+
 print('*******************')
 print('*******************')
 
-MAX_SCAN=90 # limit for testing
 scan_data = []
 
 combos = ['xyz','xzy','yxz','yzx','zxy','zyx']
@@ -18,7 +23,6 @@ total_beacons = 0
 for line in file:
 	if line[0:3]=='---': 
 		scanner = int(line.strip().split()[2])
-		if scanner >= MAX_SCAN: break
 		scan_data.append([])
 		continue
 	elif line.strip():
@@ -31,7 +35,6 @@ all_beacons = scan_data[0]
 from collections import defaultdict
 relative_distances = defaultdict(list)
 for scanner in range(0,len(scan_data)):
-	if scanner >= MAX_SCAN: break
 	for ibeacon in range(0,len(scan_data[scanner])-1):
 		for jbeacon in range(ibeacon+1,len(scan_data[scanner])):
 			a = scan_data[scanner][ibeacon]
@@ -60,7 +63,7 @@ def transform_x(point, translation, varient):
 		)
 	return res
 
-def transform(origin, point, varient):
+def add(origin, point, varient):
 	res = ( 
 		origin[0] + point[varient[1].index('x')]*varient[0][0],
 		origin[1] + point[varient[1].index('y')]*varient[0][1],
@@ -69,7 +72,7 @@ def transform(origin, point, varient):
 	return res
 
 # diff points with remapping
-def diff(A, B, varient):
+def sub(A, B, varient):
 	res = ( 
 		A[0] - B[varient[1].index('x')]*varient[0][0],
 		A[1] - B[varient[1].index('y')]*varient[0][1],
@@ -77,23 +80,74 @@ def diff(A, B, varient):
 		)
 	return res
 
-def find_scanner_transform(working):
+# used to go the opposite way to the varient
+def backtrack(point, transform):
+	n = variations[0] #neutral
+	t_rev = ([-x for x in transform[1][0]],transform[1][1]) # reverse transform
+	res = sub(transform[0],point,n)
+	return add((0,0,0),res,t_rev)
+
+def fronttrack(point, transform):
+	return add(transform[0],point,transform[1])
+
+def find_scanner_transform(matches):
+	working=defaultdict(set)
+	for (k,v) in matches: working[k].add(v)
+
+	latest = None
+# grab two points with the least matching points, so most likely right?
 	((_,first),(_,second)) = sorted([(len(working[k]),k) for k in working])[0:2]
 	for point in working[first]: 
-		for v in variations:
-			d = diff(first,point,v)
+		for v in variations: # cycle through orientations
+			d = sub(first,point,v)
 			for op in working[second]:
-				if second == transform(d,op,v):
-					return (d,v)
+				if second == add(d,op,v):
+					latest = (d,v)
+# this is janky, should really verify which is correct
+	return latest
+transformers = {}
 
 for (a,b) in potential_matches:
 	distinct_points = len(set([x for (x,y) in potential_matches[(a,b)]]))
-	if distinct_points < 12:
-		print(a,b,'not a match',distinct_points)
-	else:
-		working=defaultdict(set)
-# refactor this to dict building later
-		for (k,v) in potential_matches[(a,b)]: working[k].add(v)
+	if distinct_points >= 12:
+		trans = find_scanner_transform(potential_matches[(a,b)])
+		if trans is not None:
+			#if (b,a) == (4,1): pp.pprint(potential_matches[(a,b)])
+			routes[a].append(b)
+			routes[b].append(a)
+			transformers[(b,a)] = trans
 
-		trans = find_scanner_transform(working)
 		print('a,b relative loc', a,b,trans)
+
+"""
+for k,route in enumerate(routes):
+	print(k,route)
+	"""
+
+operations = [(2,4),(4,1),(3,1),(1,0)]
+
+"""
+debug of points for 4-1-0 in test
+debug_points = [ (515, 917, -361), (-340, -569, -846), (-460, 603, -452), (-466, -666, -811), (-355, 545, -477), (703, -491, -529), (413, 935, -424), (-391, 539, -444), (-364, -763, -893), (807, -499, -711), (755, -354, -619), (553, 889, -390), (515, 917, -361), (-466, -666, -811), (515, 917, -361), (-340, -569, -846), (-466, -666, -811), (703, -491, -529), (-364, -763, -893), (807, -499, -711), (755, -354, -619)]
+"""
+
+for op in operations:
+	(src,dst) = op
+	if op not in transformers:
+		method = backtrack
+		transform = transformers[(dst,src)]
+	else: 
+		method = fronttrack
+		transform = transformers[op]
+		
+	for point in scan_data[src]:
+		np = method(point,transform)
+		scan_data[dst].append(np)
+		if np in debug_points or point in debug_points:
+			print(method, op, point, np)
+
+pp.pprint(set(scan_data[0]))
+print(len(scan_data[0]))
+print(len(set(scan_data[0])))
+
+
