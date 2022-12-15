70b1d0e58c8c8d0e8087a97b69e8848bb35298fd
jd <jd@jdf.ie>
2022-12-15 17:27:14 +0000

    part one done

--- advent22/15/1.py
+++ advent22/15/1.py
@@ -1,47 +1,39 @@
 import pprint 
 import re
-import sys
 
 pp = pprint.PrettyPrinter(indent=2)
 
-#file = open('test','r')
+file = open('test','r')
+target_row = 10
 file = open('data','r')
+target_row = 2000000
 
 grid={}
 
 rex = r"x=(-?\d+), y=(-?\d+).*x=(-?\d+), y=(-?\d+)"
-target_row = 10
-max_x, min_x = -sys.maxsize, sys.maxsize
 
+ranges=[]
 for line in [l.strip() for l in file]:
     (sx,sy,bx,by) = [int(x) for x in re.search(rex, line).groups()]
+    radius = abs(sx-bx) + abs(sy-by)
     
-    radius = 1+ abs(sx-bx) + abs(sy-by)
-    max_x = max(sx+radius, max_x)
-    min_x = min(sx-radius, min_x)
-    for y in range(0,radius):
-        if sy+y not in grid.keys():
-            grid[sy+y] = {}
-        if sy-y not in grid.keys():
-            grid[sy-y] = {}
-        for x in range(0,radius-y):
-            grid[sy+y][sx+x] = '#'
-            grid[sy-y][sx+x] = '#'
-            grid[sy+y][sx-x] = '#'
-            grid[sy-y][sx-x] = '#'
+    if sy-radius <= target_row and target_row <= sy+radius:
+       width = radius - abs(sy-target_row) 
+       ranges.append((sx-width,sx+width))
 
-    grid[sy][sx] = 'S'
-    grid[by][bx] = 'B'
+combined = []
+ranges.sort()
+last = ranges[0]
+combined.append(last)
+for i in range(1,len(ranges)):
+    last = combined.pop()
+    if ranges[i][0] < last[1]:
+        combined.append((last[0], ranges[i][1]))
+    else:
+        combined.append(last)
+        combined.append(ranges[i])
 
-ys = list(grid.keys())
-ys.sort()
-for y in ys:
-    for x in range(min_x,max_x):
-        if x in grid[y]:
-            print(grid[y][x], end='')
-        else:
-            print('.', end='')
-    print(y)
-
-print(len([1 for g in grid[target_row] if g != 'B']))
-
+pp.pprint(combined)
+start = combined[0][0]
+end = combined.pop()[1]
+print(end-start)
