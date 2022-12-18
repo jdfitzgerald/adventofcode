445ccb3a710333293cf987c7ce889ab17ca3a877
jd <jd@jdf.ie>
2022-12-18 21:25:46 +0000

    wew! 16 working

--- advent22/16/1.py
+++ advent22/16/1.py
@@ -3,33 +3,19 @@ import re
 import os
 from functools import lru_cache
 
-@lru_cache
-def max_remaining_pressure(onvalves, minutes):
-    score = 0
-    rates = [v['rate'] for v in valves.values() if v['name'] not in onvalves]
-    rates.sort(reverse=True)
-
-    for rate in rates:
-        if minutes < 2: break
-        score += rate * (minutes -1)
-        minutes -= 2
-
-    return score
-
 def cost(a,b):
     queue = [(a,[a])]
     while queue:
         (v,path) = queue.pop()
         for t in valves[v]['tunnels'] - set(path):
             if t == b:
-                print("dist from %s to %s is"%(a,b),path)
                 return len(path)
             else:
                 queue.insert(0,(t,path+[t]))
 
 pp = pprint.PrettyPrinter(indent=2)
-filename = os.path.dirname(__file__)+'/test'
-#filename = os.path.dirname(__file__)+'/data'
+#filename = os.path.dirname(__file__)+'/test'
+filename = os.path.dirname(__file__)+'/data'
 
 file = open(filename,'r')
 
@@ -37,13 +23,13 @@ file = open(filename,'r')
 rex = re.compile(r"Valve ([A-Z]{2}) has flow rate=(\d+); tunnels? leads? to valves? (.+)")
 
 valves = {}
-# build the graph
 for line in [l.strip() for l in file]:
     [name, rate, tunnels] = rex.match(line).groups()
     tunnels = set(tunnels.split(', '))
     valve={'name':name, 'rate':int(rate), 'tunnels':tunnels}
     valves[name] = valve
 
+# build the simplified graph
 graph = {'AA':{}}
 for cur in valves:
     if valves[cur]['rate'] > 0:
@@ -58,4 +44,17 @@ for cur in valves:
 
 pp.pprint(graph)
 
-quit()
+def stepnscore(curv, minutes, path):
+    minutes -= 1
+    phere = valves[curv]['rate'] * minutes
+    pressures = [[phere,path]]
+    for v in graph[curv]:
+        if v not in path:
+            remain = minutes-(graph[curv][v])
+            if remain > 2:
+                p = stepnscore(v, remain,path + [curv])
+                pressures.append([p[0]+phere,path+[curv]])
+    return max(pressures, key=lambda x: x[0])
+        
+mins = 30
+print(stepnscore('AA',mins+1,[]))
