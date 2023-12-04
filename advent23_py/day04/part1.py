bf1d6bf1bde09539c7a1d2b27d5860a285f021da
jdfitzgerald <jd@jdf.ie>
2023-12-04 09:14:36 +0000

    part 1 done

---
+++ advent23_py/day04/part1.py
@@ -0,0 +1,14 @@
+import pprint 
+import math 
+pp = pprint.PrettyPrinter(indent=2)
+
+file = open('day04/real.data','r')
+#file = open('day04/test.data','r')
+total = 0
+for line in [l.strip() for l in file]:
+    data = line.split(':')
+    (winners,numbers) = [set(x.strip().split()) for x in data[1].strip().split('|')]
+    score = math.floor(2 ** (len(winners&numbers)-1)) # floor fixes 0.5 for -1
+    total += score
+
+print(total)
