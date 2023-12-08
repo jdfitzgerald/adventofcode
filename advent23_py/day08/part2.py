8da522b7b2d4209016f407782b63c145ba561867
jdfitzgerald <jd@jdf.ie>
2023-12-08 21:49:16 +0000

    maybe working but takes ages

--- advent23_py/day08/part2.py
+++ advent23_py/day08/part2.py
@@ -17,7 +17,7 @@ def path_gen(path):
         i+=1
 
 file = open('day08/real.data','r')
-#file = open('day08/test.data','r')
+#file = open('day08/test2.data','r')
 
 path = file.readline().strip()
 file.readline()
@@ -33,17 +33,34 @@ while line := file.readline().strip():
 steps = 0
 next_step = path_gen(path)
 
-node = nodelist['AAA']
 
+current_nodes = [node for k,node in nodelist.items() if k[2]=='A']
+
+visited = {}
+print([node.loc for node in current_nodes])
 for dir in next_step:
+    hash = ''.join(sorted([node.loc for node in current_nodes]))
+    if hash in visited:
+        print('loop')
+        break
+    visited[hash]=1
+
     steps += 1
+    z_count = 0
+    for i,node in enumerate(current_nodes):
+        if dir == 'L':
+            node = nodelist[node.left]
+        else:
+            node = nodelist[node.right]
     
-    if dir == 'L':
-        node = nodelist[node.left]
-    else:
-        node = nodelist[node.right]
-    
-    if node.loc == 'ZZZ':
+        if node.loc[2] == 'Z':
+            z_count += 1
+        current_nodes[i]=node
+
+
+    if z_count > 1:
+        print(''.join(sorted([node.loc for node in current_nodes])), z_count)
+    if z_count == len(current_nodes):
         break
 
 print(steps)
