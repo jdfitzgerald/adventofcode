3aa40df9873a83c9e99e7d6a29facbf0dc3d80fc
jdfitzgerald <jd@jdf.ie>
2023-12-08 23:25:12 +0000

    This is ugly and slow and I'm embarrassed

--- advent23_py/day05/part2.py
+++ advent23_py/day05/part2.py
@@ -1,9 +1,11 @@
 import pprint
+import time
+import functools
 pp = pprint.PrettyPrinter(indent=2)
 
 
-#file = open('day05/real.data','r')
-file = open('day05/test.data','r')
+file = open('day05/real.data','r')
+#file = open('day05/test.data','r')
 
 seeds = [int(x) for x in file.readline().strip().split()[1::]]
 seed_ranges = []
@@ -25,8 +27,11 @@ while True:
     line = file.readline().strip()
     while line:
         (dest_start, src_start, l) = [int(x) for x in line.split()]
-        r.append([dest_start, dest_start + l -1, src_start])
+        r.append((dest_start, dest_start + l -1, src_start-dest_start))
         line = file.readline().strip()
+    
+    r.sort()
+    r = tuple(r)
 
     mappings[src] = {'ranges':r, 'dest':dest}
 
@@ -38,31 +43,50 @@ while True:
 
 pp.pprint(mappings)
 
-upper_bound=57075758
-upper_bound=57075780
-upper_bound=50
+upper_bound=57075758 +1 # result from pt 1
 
-min_location = 9999999999999
+min_location = -1
+ 
+# binary search ends up ~40% faster - 1500ms vs 2500ms per 50000
+# memoization made this slower
+# realised that debug mode was actually the real timesink
+def map_value(src, v, ranges):
+    low = 0
+    high = len(ranges) -1
+    while low <= high:
+        mid = (low + high) //2
+        if  v < ranges[mid][0]:
+            high = mid -1
+        elif ranges[mid][0] <= v <= ranges[mid][1]:
+            return v + ranges[mid][2]
+        else:
+            low = mid +1
+    return v
 
 
-for location in range(34,upper_bound):
+checkpt = round(time.time() * 1000)
+
+for location in range(0,upper_bound):
     cur_value = location
     src = 'location'
 
-
     while src != 'seed':
-        for r in mappings[src]['ranges']:
-            if r[0] <= cur_value <= r[1]:
-                cur_value = r[2] + (cur_value - r[0])
-                break
+        cur_value = map_value(src,cur_value, mappings[src]['ranges'])
         src = mappings[src]['dest']
 
     for s in seed_ranges:
         if s[0] <= cur_value <= s[1]:
-            min_location = min(min_location, cur_value)
+            min_location = location
             break
 
+    if min_location > -1:
+        break
+
     if location % 50000 == 0:
+        now = round(time.time() * 1000)
+        
+        print(now - checkpt)
+        checkpt = now
         print(location)
 
 print(min_location)
