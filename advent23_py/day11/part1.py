c95117cd07183b2507c01b3603e89591ede0da2a
jdfitzgerald <jd@jdf.ie>
2023-12-11 13:56:21 +0000

    That was pretty handy - and it's much quicker without superfluous printing

--- advent23_py/day11/part1.py
+++ advent23_py/day11/part1.py
@@ -18,19 +18,22 @@ width = len(line)
 expanding_rows = {i for (i,j) in galaxies}.symmetric_difference(range(0,height))
 expanding_cols = {j for (i,j) in galaxies}.symmetric_difference(range(0,width))
 
+# change this to 1 to get part 1 answer
+# there's some off by one thing I can't be arsed figuring out
+expansion_rate = 999999 
 for g in galaxies:
-    g[0] += sum([ 1 for i in expanding_rows if i < g[0]])
-    g[1] += sum([ 1 for i in expanding_cols if i < g[1]])
+    g[0] += sum([ expansion_rate for i in expanding_rows if i < g[0]])
+    g[1] += sum([ expansion_rate for i in expanding_cols if i < g[1]])
 
 num_galaxies = len(galaxies)
 
 total_dist = 0
 for s in range(0, num_galaxies):
+    print(s)
     src = galaxies[s]
     for d in range(s, num_galaxies):
         dest = galaxies[d]
         dist = abs(src[0]-dest[0]) + abs(src[1]-dest[1])
-        print(src, dest, dist)
         total_dist += dist
 
 print(total_dist)
