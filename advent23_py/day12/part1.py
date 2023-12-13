c08c67ab73e9a3ce03331fafd5845211697b416e
jdfitzgerald <jd@jdf.ie>
2023-12-13 00:08:20 +0000

    working for test data, and did a heap to make it debugabble just now

---
+++ advent23_py/day12/part1.py
@@ -0,0 +1,55 @@
+import re
+import pprint 
+pp = pprint.PrettyPrinter(indent=2)
+
+char = r"[\?#]"
+space = r"[^#]"
+maybe_spaces = r"[^#]*"
+
+# 9659 too high
+sum = 0
+#file = open('day12/real.data','r')
+file = open('day12/test.data','r')
+for line in [l.strip() for l in file]:
+    (search,expected) = line.split(' ')
+    expected = [int(x) for x in expected.split(',')]
+
+    search = '.' + search + '.' # pad to ensure there's always a leading and trailing dot
+
+    patterns = []
+    for n in expected:
+        patterns.append(space + char + '{' + str(n) + '}')
+
+    matches = []
+    matches.append((0,search))
+
+    for (i,rex) in enumerate(patterns):
+        rex += space
+        rex = "(?=(" + rex + "))" # get a start index for possible matches
+
+        remaining = maybe_spaces.join(patterns[i+1:])
+
+        new_matches = []
+        for (offset,match) in matches:
+            m = match[offset:]
+            for r in re.finditer(rex, m):
+                start = r.span()[0]
+                post = '.' + m[start+expected[i]+2:]
+
+                replaced = match[:(offset + start)].replace('?','.')+'.' + ('#' * expected[i]) + post
+
+                if re.search(remaining,post):
+                    new_matches.append(((offset + start + expected[i] +1 ), replaced))
+
+            matches = new_matches
+
+    
+    sum += len(matches)
+
+    print(search, expected)
+    pp.pprint(matches)
+    print(len(matches))
+    print()
+
+
+print(sum)
