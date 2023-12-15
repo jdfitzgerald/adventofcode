003af9f092a361517b8bee92d5774d17157f324a
jdfitzgerald <jd@jdf.ie>
2023-12-15 22:03:51 +0000

    probably works but too slow

--- advent23_py/day14/part2.py
+++ advent23_py/day14/part2.py
@@ -1,36 +1,50 @@
 import pprint
+import copy
 pp = pprint.PrettyPrinter(indent=2)
 
 
-
-grid = []
-#file = open('day14/real.data','r')
-file = open('day14/test.data','r')
-for line in [l.strip() for l in file]:
-    grid.append(list(line))
+def score_grid(grid):
+    total = 0
+    for r in range(len(grid)):
+        multiplier = len(grid) - r
+        score = grid[r].count('O') * multiplier
+        total += score
+    return total
+def print_grid(grid):
+    for r in grid:
+        pp.pprint("".join(r))
+    print('============')
 
 
 # column-wise iteration
 def roll(grid, direction):
     # define the two ranges based on the direction and it should be easy to iterate through
     # double check order of lists - think it aint right
-    if direction == 'north':
-        outer = range(len(grid[0]))
-        inner = range(len(grid))
-    elif direction == 'south':
-        outer = range(len(grid[0]))
-        inner = reversed(range(len(grid)))
-    elif direction == 'west':
-        outer = range(len(grid))
-        inner = range(len(grid[0]))
-    else:
-        outer = range(len(grid))
-        inner = reversed(range(len(grid[0])))
-    # etc
+    width = len(grid[0])
+    height = len(grid)
 
-    for i in outer:
+    if direction in ('north', 'south'):
+        outer = range(width)
+        inner = range(height)
+    else:
+        outer = range(height)
+        inner = range(width)
+
+    def get_pos(i,j, direction):
+       if direction == 'north':
+           return (j,i)
+       elif direction == 'south':
+           return (height-j-1,i)
+       elif direction == 'west':
+           return (i,j)
+       else:
+           return (i,width-j-1)
+
+
+    for y in outer:
         queue = []
-        for j in inner:
+        for x in inner:
+            (i,j) = get_pos(y,x,direction)
             if grid[i][j] == '#':
                 queue = []
             elif grid[i][j] == 'O' and queue != []:
@@ -42,21 +56,23 @@ def roll(grid, direction):
                 queue.insert(0,(i,j)) # slot is empty
     return grid
 
+grid = []
+#file = open('day14/real.data','r')
+file = open('day14/test.data','r')
+for line in [l.strip() for l in file]:
+    grid.append(list(line))
 
-for i in range(1):
+
+for i in range(1000000000):
     grid = roll(grid, 'north')
     grid = roll(grid, 'west')
     grid = roll(grid, 'south')
     grid = roll(grid, 'east')
 
-for r in grid:
-    pp.pprint("".join(r))
+    if grid == grid1: 
+        break
+    if (i%100000) == 0:
+        print(i)
 
-# score
-total = 0
-for r in range(len(grid)):
-    multiplier = len(grid) - r
-    score = grid[r].count('O') * multiplier
-    total += score
-
-print(total)
+print_grid(grid)
+print(score_grid(grid))
