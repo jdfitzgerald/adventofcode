2aa7bd4d5f7c91fde544c6e6fa3def1a45e0770c
jdfitzgerald <jd@jdf.ie>
2023-12-16 11:02:42 +0000

    part 2 wew!

--- advent23_py/day15/part2.py
+++ advent23_py/day15/part2.py
@@ -8,12 +8,47 @@ def hash(str):
         cv = ((cv + v)*17) % 256
     return cv
 
-#file = open(os.path.dirname(__file__)+'/real.data','r')
-file = open(os.path.dirname(__file__)+'/test.data','r')
+file = open(os.path.dirname(__file__)+'/real.data','r')
+#file = open(os.path.dirname(__file__)+'/test.data','r')
+
+boxes = [{} for x in range(256)]
+boxes_idx = [0 for x in range(256)]
+
 for line in [l.strip() for l in file]:
-    total = 0
     for step in line.split(','):
-        cv = hash(step)
-        print(step, cv)
-        total += cv
-    print(total)
+        if step[-1] == '-':
+            op = '-'
+            label = step[:-1] 
+        else:
+            fl = step[-1]
+            op = '='
+            label = step[:-2]
+
+        box_no = hash(label)
+        box = boxes[box_no]
+        
+        if op == '-':
+            if label in box:
+                del box[label]
+        elif label in box:
+            box[label]['fl'] = fl
+        else:
+            idx = boxes_idx[box_no]
+            boxes_idx[box_no] += 1
+            box[label] = {'fl':fl, 'idx':idx}
+
+total = 0
+for (boxno, box) in enumerate(boxes):
+    if len(box) == 0:
+        continue
+
+    boxno += 1
+    slot = 1
+    lenses = sorted(box.items(), key=lambda x:x[1]['idx'])
+
+    for (label,lens) in lenses:
+        score = boxno * slot * int(lens['fl'])
+        slot += 1
+        total += score
+
+print(total)
