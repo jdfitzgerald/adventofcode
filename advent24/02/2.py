7eaa7d8d052f8a6c4b78bf7c6932b5c551150387
jdfitzgerald <jd@jdf.ie>
2024-12-02 15:03:34 +0000

    still not working

--- advent24/02/2.py
+++ advent24/02/2.py
@@ -2,44 +2,46 @@ import pprint
 pp = pprint.PrettyPrinter(indent=2)
 
 
-#file = open('day02/real.data','r')
-file = open('02/test.data','r')
-#file = open('02/real.data','r')
+#file = open('02/test.data','r')
+file = open('02/real.data','r')
 
-def check_list(last, last_d, row, skips):
+def check_list(row, skips):
     safe = 1
-    for i,x in row[1:]:
+
+
+    for i,x in enumerate(row):
+        if i == 0:
+            last = row[0]
+            last_d = last - row[1]
+            continue
+
         d = last - x
         if not (1 <= abs(d) <= 3) or (d*last_d < 0):
             if skips == 0:
                 skips = 1
-                if i = len(row)-1: #special case for last
-                    break
-                # skip this
-                check_list(last,last_d,row[i+1:],skips)
+                safe = check_list(row[:i]+row[(i+1):],skips)
+                if safe: return 1
 
-                # skip next
-                check_list(last,last_d,row[i+1:],skips)
+                safe = check_list(row[:(i-1)]+row[i:],skips)
+                if safe: return 1
+                else: return 0
 
                 continue
             safe = 0
             break
         last_d = d
         last = x
-    return (safe,skips)
+    return safe
 
 
 total = 0
 for line in [l.strip() for l in file]:
     row = [int(x) for x in line.split()]
-    last = row[0]
-    last_d = last - row[1]
-    skips = 0
 
-    (safe,skips) = check_list(last, last_d, row, skips)
+    safe = check_list(row,0)
 
     if safe == 0:
-        print(row, safe, skips)
+        print(row, safe)
     total = total + safe
 
 print(total)
